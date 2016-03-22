25ba6924fad71302bcf54f96b6c4ac643e038966
omgodzilla10 <trevorhoef@gmail.com>
2016-03-22 01:20:58 -0700

    (2510_assign03) Records are now stored and memory is dynamically allocated

--- 2510_assign03/asn3.c
+++ 2510_assign03/asn3.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
-#define MAXSIZE 1024
+#define MAXSIZE 256
 #define NAMESIZE 20
+#define BLOCK 2
 
 typedef struct { 
 	char last[NAMESIZE]; /* last name (1 word) */ 
@@ -15,10 +17,11 @@ typedef struct {
 	int score; /* score (between 0 & 100 inclusive) */ 
 } record;
 
-int parse_record(char entered_record[]) {
+int parse_record(char entered_record[], record *recordPtr) {
 	int i;
 	int j = 0;
 	int spaceCount = 0;
+	int leadingSpaces = 1;
 	char firstName[NAMESIZE] = "";
 	char lastName[NAMESIZE] = "";
 	char grade[4] = "";
@@ -27,22 +30,33 @@ int parse_record(char entered_record[]) {
 	name newName;
 	
 	for (i = 0; entered_record[i] != '\0'; i++) {
-		if (entered_record[i] == ' ') {
+		entered_record[i] = tolower(entered_record[i]);
+		if (i > 0 && entered_record[i] == ' ' && entered_record[i-1] == ' ') {
+			leadingSpaces = 1;
+		} else if (entered_record[i] == ' ' && !leadingSpaces) {
 			spaceCount++;
 			j = 0;
-		} else {
+		} else if (entered_record[i] != ' ') {
+			leadingSpaces = 0;
 			switch (spaceCount) {
 				case 0: firstName[j++] = entered_record[i];
 					break;
 				case 1: lastName[j++] = entered_record[i];
 					break;
 				case 2: grade[j++] = entered_record[i];
+					if (grade[j-1] == '.') {
+						return 0;
+					}
+					
 					break;
 			}
 		}
 	}
 	
 	if (spaceCount != 2)
+		return 0;
+	
+	if (strlen(firstName) > NAMESIZE || strlen(lastName) > NAMESIZE)
 		return 0;
 	
 	if (!sscanf(grade, "%d", &newRecord.score) || newRecord.score < 0 || newRecord.score > 100)
@@ -55,18 +69,43 @@ int parse_record(char entered_record[]) {
 		return 0;
 	
 	newRecord.name = newName;
+	*recordPtr = newRecord;
 	
 	return 1;
 }
 
-int main (void) {
-	char entered_string[MAXSIZE] = "";
-	while(fgets(entered_string, MAXSIZE, stdin)) {
-		if (!parse_record(entered_string))
-			/* temp */
-			fprintf(stderr, "Invalid record entered.");
+void display_records(record *recordList, size_t numAllocated) {
+	size_t i;
+	for (i = 0; i < numAllocated; i++) {
+		printf("\n%s %s %d", recordList[i].name.last, recordList[i].name.first, recordList[i].score);
 	}
-	printf(entered_string);
+}
+
+int main (void) {
+	size_t numAllocated = 0;
+	size_t numUsed = 0;
+	char entered_string[MAXSIZE] = "";
+	record *tempList;
+	record *recordList;
+	record newRecord;
+	
+	while(fgets(entered_string, MAXSIZE, stdin)) {
+		if (numUsed == numAllocated) {
+			if (!(tempList = realloc(recordList, (numAllocated + BLOCK) * sizeof(record)))) {
+				fprintf(stderr, "Failed to allocate additional memory.");
+				return 0;
+			}
+			
+			recordList = tempList;
+			numAllocated += BLOCK;
+		}
+		
+		if(parse_record(entered_string, &newRecord)) {
+			recordList[numUsed++] = newRecord;
+		}
+	}
+	
+	display_records(recordList, numUsed);
 	
 	return 1;
 }
